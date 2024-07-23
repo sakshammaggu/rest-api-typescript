0e56d8f0bdfeb84bf41df37df1942b14d6282b13
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-24 01:14:01 +0530

    dbUrl in .env file

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
     port: "8000",
-    dbUrl: "mongodb+srv://sakshammaggu80:Drb5F6GeRdNbM4y4@rest-api-typescript.vo3sw6l.mongodb.net/?retryWrites=true&w=majority&appName=rest-api-typescript"
+    dbUrl: process.env.PUBLIC_MONGODB_URL,
 }
