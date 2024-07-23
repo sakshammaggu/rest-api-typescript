8839e1541d689274408eb6c7b8f6c5512fd7566f
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-24 00:27:46 +0530

    starting a server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import express from "express";
+import config from 'config';
+
+const port=config.get('port');
+const app=express();
+
+app.listen(port, ()=>{
+    console.log(`App is running at port ${port}`);
+});
