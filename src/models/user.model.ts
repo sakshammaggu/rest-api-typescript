ed923659a85e4e59d5564e91fb66b65e8b7abd3b
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-25 03:11:20 +0530

    create user controller and user service

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -46,6 +46,6 @@ userSchema.methods.comparePassword=async function(candidatePassword: string): Pr
     return bcrypt.compare(candidatePassword, user.password).catch((e)=>false)
 }
 
-const User=mongoose.model("User", userSchema);
+const UserModel=mongoose.model("User", userSchema);
 
-export default User;
+export default UserModel;
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
 const routes=(app: Express)=>{
     app.get('/healthCheck', (req: Request, res: Response)=>{
         res.status(200).send('OK');
     });
+
+    app.post('/api/users', createUserHandler);
 };
 
 export default routes;
